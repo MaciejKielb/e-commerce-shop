d605acbf6485952f885af9368576bbbcccdc7f30
Maciej Kielb <kielbaldo@gmail.com>
2026-01-27 12:30:57 +0200

    refactor: remove unused imports

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer, useMemo, useCallback } from 'react';
+import { createContext, useReducer } from 'react';
 
 import { createAction } from '../utils/reducer/reducer.utils';
 
